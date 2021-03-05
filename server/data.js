97ca1476e3ef39918626521ef0c06a160aef9f90
Gracie Ingermanson <gmi@umich.edu>
2021-03-05 13:26:15 -0500

    feat(server/index, server/data): add HTTP routes for getting balances, adding a timestamped transaction, and spending points; add spend() function, update addTransaction() function to keep track of which points are spent vs. unspent

--- server/data.js
+++ server/data.js
@@ -1,5 +1,5 @@
-// Store list of all transactions in chronological order
-const TRANSACTIONS = [];
+// Store total number of points for the user
+let TOTAL_POINTS = 0;
 
 // Store balances for all payers
 // This will look like the following:
@@ -7,22 +7,155 @@ const TRANSACTIONS = [];
 //  'UNILEVER': {payer: 'UNILEVER', balance: 50}}
 const BALANCES = {};
 
-const addTransaction = (transaction) => {
-  let index = 0;
-  // search through TRANSACTIONS to find where to insert the new
-  // transaction to maintain chronological order
-  while (
-    index < TRANSACTIONS.length &&
-    transaction.timestamp > TRANSACTIONS[index].timestamp
-  )
-    index++;
-  // insert the new transaction into TRANSACTIONS
-  TRANSACTIONS.splice(index, 0, transaction);
+// Store list of all transactions in chronological order
 
+// Each stored transaction will have the form
+// {payer:<string>, points:<integer>, timestamp:<Date>, isCredit:<boolean>,
+// unspentPoints:<integer>}
+
+// The field unspentPoints has a value between 0 and points
+// We will maintain the following two invariants:
+//    (1) Regardless of the sequence of calls to addTransaction(), if the
+//        balance stored for a given payer is nonnegative then every debit
+//        transaction has field unspentPoints === 0
+//    (2) Given two credit transactions transaction1 and transaction2
+//        where transaction1.timestamp < transaction2.timestamp, if
+//        transaction2.unspentPoints < transaction2.points then
+//        transaction1.unspentPoints === 0
+const TRANSACTIONS = [];
+
+// Called when addTransaction is called for a credit transaction
+// Updates the unspentPoints fields of debits with the same payer
+// as well as credits with the same payer and later timestamps
+const settleBalancesForNewCredit = (
+  transaction,
+  debitsWithSamePayer,
+  creditsWithSamePayer
+) => {
+  // Settle any outstanding debits
+  let index = 0;
+  while (index < debitsWithSamePayer.length && transaction.unspentPoints > 0) {
+    const otherTransaction = debitsWithSamePayer[index];
+    if (otherTransaction.unspentPoints < 0) {
+      const diff = Math.min(
+        transaction.unspentPoints,
+        Math.abs(otherTransaction.unspentPoints)
+      );
+      otherTransaction.unspentPoints += diff;
+      transaction.unspentPoints -= diff;
+    }
+    index++;
+  }
+  // Points with an earlier timestamp should be spent first, so if there are
+  // unspent points for the new credit transaction, iterate backward through
+  // the credits with later timestamps and refund points
+  index = creditsWithSamePayer.length - 1;
+  while (
+    index >= 0 &&
+    transaction.unspentPoints > 0 &&
+    creditsWithSamePayer[index].timestamp >= transaction.timestamp
+  ) {
+    const otherTransaction = creditWithSamePayer[index];
+    if (otherTransaction.unspentPoints < otherTransaction.points) {
+      const diff = Math.min(
+        otherTransaction.points - otherTransaction.unspentPoints,
+        transaction.unspentPoints
+      );
+      otherTransaction.unspentPoints += diff;
+      transaction.unspentPoints -= diff;
+    }
+  }
+};
+
+// Called when addTransaction is called for a new debit
+// Updates the unspentPoints fields for credit transactions with the same payer
+const settleBalancesForNewDebit = (transaction, creditsWithSamePayer) => {
+  let index = 0;
+  while (index < creditsWithSamePayer.length && transaction.unspentPoints < 0) {
+    const otherTransaction = creditsWithSamePayer[index];
+    if (otherTransaction.unspentPoints > 0) {
+      const diff = Math.min(
+        Math.abs(transaction.unspentPoints),
+        otherTransaction.unspentPoints
+      );
+      transaction.unspentPoints += diff;
+      otherTransaction.unspentPoints -= diff;
+    }
+    index++;
+  }
+};
+
+// Add a transaction and do any necessary bookkeeping
+const addTransaction = (transaction) => {
+  // Update total number of points
+  TOTAL_POINTS += transaction.points;
   // update the balance of the payer for the transaction
   if (!BALANCES[transaction.payer]) {
     BALANCES[payer] = { payer, balance: transaction.balance };
   } else {
     BALANCES[payer].balance += transaction.balance;
   }
+
+  // Get a list of other transactions with the same payer, for bookkeeping purposes
+  const transactionsWithSamePayer = TRANSACTIONS.filter(
+    otherTransaction.payer === transaction.payer
+  );
+  // Split up into credits and debits
+  const debitsWithSamePayer = transactionsWithSamePayer.filter(
+    !otherTransaction.isCredit
+  );
+  const creditsWithSamePayer = transactionsWithSamePayer.filter(
+    otherTransaction.isCredit
+  );
+
+  if (transaction.isCredit) {
+    settleBalancesForNewCredit(
+      transaction,
+      debitsWithSamePayer,
+      creditsWithSamePayer
+    );
+  } else {
+    settleBalancesForNewDebit(transaction, creditsWithSamePayer);
+  }
+
+  // search through TRANSACTIONS to find where to insert the new
+  // transaction to maintain chronological order
+  let index = 0;
+  while (index < TRANSACTIONS.length && TRANSACTIONS[index].timestamp) index++;
+
+  // insert the new transaction into TRANSACTIONS
+  TRANSACTIONS.splice(index, 0, transaction);
+};
+
+//  We assume that TRANSACTIONS and BALANCES are up-to-date and all timestamps are in the past
+// Throw an error if the total number of points is insufficient
+const spend = (request) => {
+  if (TOTAL_POINTS < request.points) {
+    throw new Error('Insufficient points');
+  }
+  TOTAL_POINTS -= request.points;
+  let points = request.points;
+  let index = 0;
+  let debits = {};
+  let additionalTransactions = [];
+  const transactionsWithPoints = TRANSACTIONS.filter(
+    (transaction) => transaction.unspentPoints > 0
+  );
+  while (points > 0) {
+    const transaction = transactionsWithPoints[index];
+    const diff = Math.min(transaction.unspentPoints, points);
+    additionalTransactions.push({
+      payer: transaction.payer,
+      points: -diff,
+      timestamp: new Date(),
+    });
+    transaction.unspentPoints -= diff;
+    points -= diff;
+    if (!debits[transaction.payer]) {
+      debits[transaction.payer] = { payer: transaction.payer, points: 0 };
+    }
+    debits[transaction.payer].points -= diff;
+    index++;
+  }
+  return Object.values(debits);
 };
--- server/index.js
+++ server/index.js
@@ -1,2 +1,36 @@
+require('./data');
+const PORT = 8080;
 
+const express = require('express');
+const app = express();
 
+app.get('/balance', (req, res, next) => {
+  res.json(Object.values(BALANCES));
+});
+
+app.post('/transaction', (req, res, next) => {
+  const { payer, points, timestamp } = req;
+  const transaction = { payer, points, timestamp };
+  transaction.points = parseInt(points);
+  if (transaction.points !== 0) {
+    transaction.timestamp = new Date(timestamp);
+    transaction.isCredit = transaction.points > 0;
+    transaction.unspentPoints = transaction.points;
+    addTransaction(transaction);
+  }
+  res.sendStatus(200);
+});
+
+app.post('/spend', (req, res, next) => {
+  const { points } = req;
+  const spendRequest = { points };
+  spendRequest.points = parseInt(points);
+  try {
+    const spendResponse = spend(spendRequest);
+    res.json(spendResponse);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
